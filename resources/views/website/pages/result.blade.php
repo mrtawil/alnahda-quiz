5d0497795ab984871d3478d6ae71047889af9b7b
Abed Tawil <aboditawil@gmail.com>
2022-02-23 20:40:45 +0200

    start with result page

--- resources/views/website/pages/result.blade.php
+++ resources/views/website/pages/result.blade.php
@@ -1,5 +1,9 @@
 @extends('website.layouts.app')
 
 @section('content')
-    Result
+    <div class="result page">
+        <div class="intro-img-container">
+            <img class="intro-img" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+        </div>
+    </div>
 @endsection
