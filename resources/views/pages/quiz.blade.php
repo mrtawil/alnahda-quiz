78a1c666244200f440fe2ddfa4721660326f8b12
Abed Tawil <aboditawil@gmail.com>
2022-02-24 20:32:07 +0200

    add ids to questions and ids

--- resources/views/pages/quiz.blade.php
+++ resources/views/pages/quiz.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="quiz page">
-        <div class="question-container fade-in" id="question-container">
+        <div class="question-container fade-in" id="question-container" :data-question-id="question.id">
             <div class="question-counter">@{{ quiz.questions.length }}/@{{ question_index + 1 }}</div>
             <div class="question-text-container">
                 <div class="question-text">@{{ question.text_ar }}</div>
             </div>
             <div class="question-options-container">
-                <div class="option-container" :id="'option-container-'+index" v-for="(option, index) in question.options" v-on:click="onNextQuestion(index)">
+                <div class="option-container" :id="'option-container-'+index" :data-option-id="question.id" v-for="(option, index) in question.options" v-on:click="onNextQuestion(index)">
                     <span class="option-counter">@{{ index + 1 }}</span>
                     <span class="option-text">@{{ option.text_ar }}</span>
                 </div>
